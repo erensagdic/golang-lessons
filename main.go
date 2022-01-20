e2215b65d9b58ca3a0f47a7805c622c06398a7d2
Eren Sağdıç <eren@saveas.com.tr>
2022-01-20 23:11:21 +0300

    Golang first lessons codes for about echo framework and request response with echo.

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
+	"net/http"
+)
+
+var e = echo.New()
 
 func main() {
-	fmt.Println("First code for first commit.")
+	e.GET("/", mainHandler)
+	e.GET("/users", usersHandler)
+	e.POST("/login", loginHandler)
+	runServer()
+}
+
+func loginHandler(context echo.Context) error {
+	username := context.FormValue("username")
+	nameSurname := context.FormValue("nameSurname")
+	email := context.FormValue("email")
+	password := context.FormValue("password")
+
+	if (username == "erensagdicc" || email == "erensagdicc@gmail.com") && password == "erensagdicc123" {
+		response := "Hoş Geldin " + nameSurname
+
+		return context.String(http.StatusOK, response)
+	}
+
+	return context.String(http.StatusForbidden, "Buraya girmeye yetkiniz yok.")
+}
+
+func mainHandler(context echo.Context) error {
+	return context.String(http.StatusOK, "Welcome to home page.")
+}
+
+func usersHandler(context echo.Context) error {
+	username := context.QueryParam("username")
+	nameSurname := context.QueryParam("nameSurname")
+	email := context.QueryParam("email")
+	password := context.QueryParam("password")
+
+	if (username == "erensagdicc" || email == "erensagdicc@gmail.com") && password == "erensagdicc123" {
+		response := "Hoş Geldin " + nameSurname
+
+		return context.String(http.StatusOK, response)
+	}
+
+	return context.String(http.StatusForbidden, "Buraya girmeye yetkiniz yok.")
+
+}
+
+func runServer() {
+	if err := e.Start(":8080"); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
